9a562ad010e7b5999db0e62a90eaa97190385fff
Быкова Екатерина Алексеевна <EAleksBykova@sberbank.ru>
2023-01-16 12:37:10 +0300

    ready with security on .ftl templates

--- src/main/java/com/example/messenger/controller/MainController.java
+++ src/main/java/com/example/messenger/controller/MainController.java
@@ -1,7 +1,5 @@
 package com.example.messenger.controller;
 
-import static java.awt.SystemColor.text;
-
 import com.example.messenger.domain.Message;
 import com.example.messenger.domain.User;
 import com.example.messenger.repos.MessageRepository;
@@ -9,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,10 +23,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepository.findAll();
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -35,27 +41,13 @@ public class MainController {
     public String add(
         @AuthenticationPrincipal User user,
         @RequestParam String text,
-        @RequestParam String tag,
-        Map<String, Object> model) {
-
+        @RequestParam String tag, Map<String, Object> model
+    ) {
         Message message = new Message(text, tag, user);
+
         messageRepository.save(message);
 
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
-
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map <String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
 
         model.put("messages", messages);
 
--- src/main/java/com/example/messenger/domain/Message.java
+++ src/main/java/com/example/messenger/domain/Message.java
@@ -11,7 +11,7 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Message {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
     private String text;
@@ -21,18 +21,19 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User author;
 
-    public Message(String text, String tag, User user) {
-        this.text = text;
-        this.tag = tag;
-        this.author = user;
+    public Message() {
     }
 
-    public Message() {
+    public Message(String text, String tag, User user) {
+        this.author = user;
+        this.text = text;
+        this.tag = tag;
     }
 
     public String getAuthorName() {
         return author != null ? author.getUsername() : "<noname>";
     }
+
     public User getAuthor() {
         return author;
     }
@@ -41,20 +42,12 @@ public class Message {
         this.author = author;
     }
 
-    public String getText() {
-        return text;
-    }
-
     public void setText(String text) {
         this.text = text;
     }
 
-    public String getTag() {
-        return tag;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
+    public String getText() {
+        return text;
     }
 
     public Integer getId() {
@@ -63,5 +56,13 @@ public class Message {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
     }
 }
